5eb4a2ae680d78ca37617b91c2dce2aa529c5ccd
Anand Chaudhary <wassolibhai420@gmail.com>
2026-04-28 14:09:02 +0545

    feat: add return statement support and corresponding tests

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -9,7 +9,7 @@ import (
 
 func TestLetStatements(t *testing.T) {
 	input := `
-		let x 5;
+		let x = 5;
 		let y = 10;
 		let foobar = 8383838;
 	`
@@ -84,4 +84,35 @@ func checkParserErrors(t *testing.T, p *Parser) {
 	}
 
 	t.FailNow()
+}
+
+func TestReturnStatements(t *testing.T){
+	input := `
+	return 5; 
+	return 10;
+	return 993322;
+	`
+
+	l := lexer.New(input)
+	p := New(l)
+
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
+	if len(program.Statements) != 3 {
+		t.Fatalf("program.Statements does not contain 3 statements. got=%d", len(program.Statements))
+	}
+
+	for _, stmt := range program.Statements {
+		returnStmt, ok := stmt.(*ast.ReturnStatement)
+
+		if !ok {
+			t.Errorf("stmt not *ast.returnStatement. got=%T", stmt)
+			continue
+		}
+
+		if returnStmt.TokenLiteral() != "return" {
+			t.Errorf("returnStmt. TokenLiteral not 'return', got %q", returnStmt.TokenLiteral())
+		}
+	}
 }
